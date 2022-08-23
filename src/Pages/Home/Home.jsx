f53c8d4def5a06708ef050bba065d46bfe76ec5c
atharvapatil22 <atharva.22.patil@gmail.com>
2022-08-24 02:19:34 +0530

    Linked api and designed results screen

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,24 +2,33 @@ import axios from "axios";
 import React, { useState } from "react";
 import styles from "./Home.module.css";
 import { BaseURL } from "../../environment";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   const [inputHtml, setInputHtml] = useState("");
 
   const handleSubmit = () => {
+    if (!inputHtml) {
+      alert("Please enter some valid html!");
+      return;
+    }
+
     axios
       .post(BaseURL + "/parse-html", { inputHtml: inputHtml })
       .then((res) => {
-        console.log("Res", res);
+        console.log("Res", res.data);
+        navigate("results", { state: { nodeList: res.data } });
       })
       .catch((err) => {
+        alert("Some error occured");
         console.log("Err", err);
       });
   };
 
   return (
     <div className={styles.wrapper}>
-      <h1>Find Similar Nodes</h1>
+      <h1 className="font-bold">Find Similar Nodes</h1>
       <h3>Paste HTML in textbox below</h3>
 
       <textarea
--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import ResultCard from "../../Components/ResultCard/ResultCard";
 import styles from "./Results.module.css";
 
 function Results() {
-  return <div>Results</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const nodeList = location.state.nodeList;
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <h1 className="font-bold">Results</h1>
+        <button onClick={() => navigate(-1)}>Run Again</button>
+      </div>
+      {nodeList.length === 0 ? (
+        <p className={styles.message}>No Duplicates found</p>
+      ) : (
+        <div
+          style={{
+            width: "100%",
+          }}
+        >
+          {nodeList.map((item, index) => (
+            <div
+              key={index}
+              className={styles.card_container}
+              style={{
+                justifyContent: index % 2 == 0 ? "start" : "end",
+              }}
+            >
+              <ResultCard data={item} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Results;
